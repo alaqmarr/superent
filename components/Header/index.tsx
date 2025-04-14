0b5599fe11ba75fc5633d2a79593f65f7b23c398
Al Aqmar <alaqmarak0810@gmail.com>
2025-04-14 13:18:34 +0530

    contact

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -156,7 +156,7 @@ const Header = () => {
 
 
             <Link
-              href="https://nextjstemplates.com/templates/solid"
+              href="https://wa.me/+919740242725"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
               Contact Us
