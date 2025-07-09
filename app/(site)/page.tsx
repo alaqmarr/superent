18e3268d5d7d020547fb90f6d4c12f3892af188f
Al Aqmar <alaqmarak0810@gmail.com>
2025-07-09 17:15:04 +0530

    ff hidde

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
       <About />
       {/* <FeaturesTab /> */}
       <Blog />
-      <FunFact />
+      {/* <FunFact /> */}
       {/* <Integration /> */}
       <CTA />
       {/* <FAQ /> */}
