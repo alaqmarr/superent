60bd03c3f65fded3e0fb76623001cfe834b0398a
Al Aqmar <alaqmarak0810@gmail.com>
2025-07-09 17:14:39 +0530

    cta change

--- components/CTA/index.tsx
+++ components/CTA/index.tsx
@@ -60,7 +60,7 @@ const CTA = () => {
                 />
                 <a
                   href="tel:
-9740242725"
+8886320552"
                   className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white hover:opacity-90 dark:bg-white dark:text-black"
                 >
                   Contact Us
